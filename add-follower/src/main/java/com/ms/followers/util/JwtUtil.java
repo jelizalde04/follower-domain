254643af7f7ee10580a2fc580f4b26709b5d110f
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-22 19:02:51 -0500

    feat (security): 🔒 Add JWT utility class

---
+++ add-follower/src/main/java/com/ms/followers/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.ms.follower.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                   .setSigningKey(secretKey)
+                   .parseClaimsJws(token)
+                   .getBody()
+                   .getSubject();
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                   .setSubject(username)
+                   .setIssuedAt(new Date())
+                   .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))  // 1 hora
+                   .signWith(SignatureAlgorithm.HS256, secretKey)
+                   .compact();
+    }
+}
