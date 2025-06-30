c82a4795a86e3b8cd99307077d6f364eefbb6e53
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-29 23:22:48 -0500

    chore(config): 🔧 update application configuration

--- add-follower/config/environments/production.rb
+++ add-follower/config/environments/production.rb
@@ -78,10 +78,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+   config.hosts = [ pethub-lb-1937068389.us-east-1.elb.amazonaws.com ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
